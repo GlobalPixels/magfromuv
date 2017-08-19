db1b1aec0d6b0b420335dacbaa5d02190cc23898
Zac Flamig <Zac@WeatherWary.com>
2017-08-19 00:39:40 -0500

    Remove old code

--- TifGrid.h
+++ TifGrid.h
@@ -6,6 +6,5 @@
 FloatGrid *ReadFloatTifGrid(const char *file, double top, double bottom, double left, double right, bool *outside = NULL);
 FloatGrid *ReadFloatTifGrid(const char *file, FloatGrid *incGrid, double top, double bottom, double left, double right, bool *outside = NULL);
 void WriteFloatTifGrid(const char *file, FloatGrid *grid, const char *artist = NULL, const char *datetime = NULL, const char *copyright = NULL);
-LongGrid *ReadLongTifGrid(const char *file);
 
 #endif
